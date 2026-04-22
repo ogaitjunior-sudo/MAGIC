ad5937626ee923bc834dca17da2a89ea841c1ada
Tiago Reis <ogaitjunior@gmail.com>
2026-04-22 20:58:46 -0300

    ajuste final para deploy vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
+import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    tsConfigPaths({ projects: ["./tsconfig.json"] }),
-    tanstackStart(),
-    nitro({
-      preset: "vercel",
-      vercel: {
-        functions: {
-          runtime: "nodejs22.x",
-        },
-      },
-    }),
-    viteReact(),
-  ],
+  plugins: [react(), tailwindcss(), tsConfigPaths({ projects: ["./tsconfig.json"] })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    dedupe: ["react", "react-dom", "@tanstack/react-router", "@tanstack/react-start"],
+    dedupe: ["react", "react-dom", "@tanstack/react-router"],
   },
 });
